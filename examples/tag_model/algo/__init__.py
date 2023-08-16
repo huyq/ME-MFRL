85bd5608bb8ca4af52cd3113f6e6b7da6a339909
Hu_Yuan_Quan_ <26775-Hu_Yuan_Quan_@users.noreply.gitlab.aicrowd.com>
2023-08-16 23:43:27 +0800

    fix some bugs

--- examples/tag_model/algo/__init__.py
+++ examples/tag_model/algo/__init__.py
@@ -4,7 +4,8 @@ from . import sac
 
 
 PPO = ppo.PPO
-MFPPO = ppo.MFPPO
+ME_MFPPO = ppo.ME_MFPPO
+GRID_MFPPO = ppo.GRID_MFPPO
 SAC = sac.SAC
 
 
@@ -12,9 +13,9 @@ def spawn_ai(algo_name, sess, env, handle, human_name, max_steps, order=4):
     if algo_name == 'ppo':
         model = PPO(sess, human_name, handle, env)
     elif algo_name == 'me_mfppo':
-        model = MFPPO(sess, human_name, handle, env, order=order)
+        model = ME_MFPPO(sess, human_name, handle, env, order=order)
     elif algo_name == 'grid_mfppo':
-        model = MFPPO(sess, human_name, handle, env, order=order)
+        model = GRID_MFPPO(sess, human_name, handle, env, order=order)
     elif algo_name == 'sac':
         model = SAC(sess, human_name, handle, env, max_steps, memory_size=80000)
     return model
